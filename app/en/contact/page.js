81d227905c9e6edb6cec1bca7501d748f059e26c
Tentaro Kubo <39017717+OriginalTT@users.noreply.github.com>
2023-09-29 00:59:05 -0400

    Add disabled state to form button.

--- app/en/contact/page.js
+++ app/en/contact/page.js
@@ -1,16 +1,25 @@
 'use client';
 import emailjs from '@emailjs/browser';
 import { useRef } from 'react';
-import { motion, spring } from 'framer-motion';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
 
 const contact = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const areFieldsFilled = () => {
+        return name !== '' && email !== '' && message !== '';
+    };
+
     const form = useRef();
     const sendEmail = e => {
         e.preventDefault();
 
         emailjs.sendForm(process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID, process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_ID, form.current, process.env.NEXT_PUBLIC_EMAIL_PUBLIC_KEY)
             .then((result) => {
-                window.alert('Success! Thanks for reaching out.\n連絡承りました。返信までしばらくお待ちください。');
+                window.alert('Success! Thanks for reaching out.\nYou will recieve an email soon.');
             }, (error) => {
                 window.alert('Something went wrong. Try again!\nエラーが起きました。しばらくしてから再度お試しください。');
             });
@@ -32,31 +41,38 @@ const contact = () => {
                 className="p-10 w-[500px] rounded-xl flex flex-col items-center gap-5 mx-10 my-10
                 bg-highlight2"
                 ref={form} onSubmit={sendEmail}>
+                <p className='ml-auto text-xs text-red-400'>* Required</p>
                 <div>
-                    <label htmlFor="name">Name</label> <br />
+                    <label htmlFor="name">Name <span className='text-red-400'>*</span></label> <br />
                     <input
                         className="bg-transparent p-2 border-offwhite border rounded-xl w-[400px]"
-                        type="text" id="name" name="name" required />
+                        type="text" id="name" name="name" required
+                        onChange={(e) => setName(e.target.value)} />
                 </div>
 
                 <div>
-                    <label htmlFor="email">Email</label> <br />
+                    <label htmlFor="email">Email <span className='text-red-400'>*</span></label> <br />
                     <input
                         className="bg-transparent p-2 border-offwhite border rounded-xl w-[400px]"
-                        type="email" id="email" name="email" required />
+                        type="email" id="email" name="email" required
+                        onChange={(e) => setEmail(e.target.value)} />
                 </div>
 
                 <div>
-                    <label htmlFor="message">Message</label> <br />
+                    <label htmlFor="message">Message <span className='text-red-400'>*</span></label> <br />
                     <textarea
                         className="bg-transparent p-2 border-offwhite border rounded-xl w-[400px]"
-                        id="message" name="message" rows="4" required></textarea>
+                        id="message" name="message" rows="4" required
+                        onChange={(e) => setMessage(e.target.value)}></textarea>
                 </div>
 
                 <div>
                     <button type="submit" value={'Send'}
-                        className="bg-offwhite text-highlight rounded-full px-5 py-3 drop-shadow-md ml-[300px]
-                        box-border border-2 border-offwhite hover:text-offwhite hover:bg-highlight">Submit</button>
+                        className={`bg-offwhite text-highlight rounded-full px-5 py-3 drop-shadow-md ml-[300px]
+                        box-border border-2 border-offwhite disabled:opacity-30 disabled:cursor-not-allowed
+                        ${!areFieldsFilled() ? '' : 'hover:text-offwhite hover:bg-highlight'}`}
+                        disabled={!areFieldsFilled()}
+                    >Submit</button>
                 </div>
             </motion.form>
         </main>
--- app/jp/contact/page.js
+++ app/jp/contact/page.js
@@ -1,18 +1,27 @@
 'use client';
 import emailjs from '@emailjs/browser';
 import { useRef } from 'react';
-import { motion, spring } from 'framer-motion';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
 
 const contact = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const areFieldsFilled = () => {
+        return name !== '' && email !== '' && message !== '';
+    };
+
     const form = useRef();
     const sendEmail = e => {
         e.preventDefault();
 
         emailjs.sendForm(process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID, process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_ID, form.current, process.env.NEXT_PUBLIC_EMAIL_PUBLIC_KEY)
             .then((result) => {
-                window.alert('Success! Thanks for reaching out.\n連絡承りました。返信までしばらくお待ちください。');
+                window.alert('ご連絡承りました。返信までしばらくお待ちください。');
             }, (error) => {
-                window.alert('Something went wrong. Try again!\nエラーが起きました。しばらくしてから再度お試しください。');
+                window.alert('エラーが起きました。しばらくしてから再度お試しください。');
             });
     };
 
@@ -32,31 +41,38 @@ const contact = () => {
                 className="p-10 w-[500px] rounded-xl flex flex-col items-center gap-5 mx-10 my-10
                 bg-highlight2"
                 ref={form} onSubmit={sendEmail}>
+                <p className='ml-auto text-xs text-red-400'>* 必須項目</p>
                 <div>
-                    <label htmlFor="name">お名前</label> <br />
+                    <label htmlFor="name">お名前 <span className='text-red-400'>*</span></label> <br />
                     <input
                         className="bg-transparent p-2 border-offwhite border rounded-xl w-[400px]"
-                        type="text" id="name" name="name" required />
+                        type="text" id="name" name="name" required
+                        onChange={(e) => setName(e.target.value)} />
                 </div>
 
                 <div>
-                    <label htmlFor="email">メールアドレス</label> <br />
+                    <label htmlFor="email">メールアドレス <span className='text-red-400'>*</span></label> <br />
                     <input
                         className="bg-transparent p-2 border-offwhite border rounded-xl w-[400px]"
-                        type="email" id="email" name="email" required />
+                        type="email" id="email" name="email" required
+                        onChange={(e) => setEmail(e.target.value)} />
                 </div>
 
                 <div>
-                    <label htmlFor="message">メッセージ</label> <br />
+                    <label htmlFor="message">メッセージ <span className='text-red-400'>*</span></label> <br />
                     <textarea
                         className="bg-transparent p-2 border-offwhite border rounded-xl w-[400px]"
-                        id="message" name="message" rows="4" required></textarea>
+                        id="message" name="message" rows="4" required
+                        onChange={(e) => setMessage(e.target.value)}></textarea>
                 </div>
 
                 <div>
                     <button type="submit" value={'Send'}
-                        className="bg-offwhite text-highlight rounded-full min-w-[100px] px-5 py-3 drop-shadow-md ml-[300px]
-                        box-border border-2 border-offwhite hover:text-offwhite hover:bg-highlight">送信</button>
+                        className={`bg-offwhite text-highlight rounded-full min-w-[100px] px-5 py-3 drop-shadow-md ml-[300px]
+                        box-border border-2 border-offwhite disabled:opacity-30 disabled:cursor-not-allowed
+                        ${!areFieldsFilled() ? '' : 'hover:text-offwhite hover:bg-highlight'}`}
+                        disabled={!areFieldsFilled()}
+                    >送信</button>
                 </div>
             </motion.form>
         </main>
