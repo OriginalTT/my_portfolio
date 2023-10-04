9f4a862b0fc7ef991ca0845d6b0a9e48618a9fd2
Tentaro Kubo <39017717+OriginalTT@users.noreply.github.com>
2023-10-03 22:47:07 -0400

    Add english meta data

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,10 @@ const notoSansJP = Noto_Sans_JP({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>Tentaro Kubo's Portfolio</title>
+        <meta name="description" content="Tentaro Kubo is A highly motivated UX designer and front-end developer with a background from the University of Waterloo, where he specialized in UX design, video production, and graphic design, complemented by a minor in Computing. Drawing from his experiences in Japan and Canada, he brings a unique international perspective to his work and communicates seamlessly in both Japanese and English. He operates under the principle of logical creativity, ensuring designs that are both innovative and user-friendly." />
+      </head>
       <body className={`${notoSansJP.className} bg-highlight text-offwhite tracking-tight`}>
         {children}
       </body>
